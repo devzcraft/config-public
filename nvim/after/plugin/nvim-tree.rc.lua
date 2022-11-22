1d22757ed6e3478af1e180219f16f34c0d7c68e3
Marian Stanciuc <dagersic@gmail.com>
2022-11-22 09:34:36 +0200

    Fix mason and nvim-tree

---
+++ nvim/after/plugin/nvim-tree.rc.lua
@@ -0,0 +1,16 @@
+local status, tree = pcall(require, "nvim-tree")
+
+if not status then
+	return
+end
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+tree.setup()
+
+vim.keymap.set("n", "<M-1>", "<CMD>NvimTreeToggle<CR>")
+vim.keymap.set("n", "<C-\\>", "<CMD>NvimTreeFindFile<CR>")
